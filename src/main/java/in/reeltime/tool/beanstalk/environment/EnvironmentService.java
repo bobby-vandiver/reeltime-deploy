cc91be0e235d1a279926a089396dfd180235fead
Bobby Vandiver <bobby.vandiver88@gmail.com>
2016-06-16 21:29:04 -0400

    Update solution stack name

--- src/main/java/in/reeltime/tool/beanstalk/environment/EnvironmentService.java
+++ src/main/java/in/reeltime/tool/beanstalk/environment/EnvironmentService.java
@@ -64,7 +64,7 @@ public class EnvironmentService {
 
     public EnvironmentDescription createEnvironment(String environmentName, String applicationName, String versionLabel,
                                                     BeanstalkConfiguration beanstalkConfiguration) {
-        String solutionStack = "64bit Amazon Linux 2015.09 v2.0.8 running Tomcat 8 Java 8";
+        String solutionStack = "64bit Amazon Linux 2016.03 v2.1.1 running Tomcat 8 Java 8";
 
         // The application server will always be a web server
         EnvironmentTier tier = new EnvironmentTier()
